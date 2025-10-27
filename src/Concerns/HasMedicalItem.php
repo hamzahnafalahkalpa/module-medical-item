9a01fe1f5bf8974b798d8f2b29c2d1002b38cbcd
hamzah <hamzahnafalah@gmail.com>
2025-10-27 14:22:51 +0700

    update docker

--- src/Concerns/HasMedicalItem.php
+++ src/Concerns/HasMedicalItem.php
@@ -7,10 +7,17 @@ trait HasMedicalItem
     public static function bootHasMedicalItem()
     {
         static::created(function ($query) {
-            $query->medicalItem()->firstOrCreate([
-                'name' => $query->name
-            ]);
+            if ($query->isUsingMedicalItem()){
+                $query->medicalItem()->firstOrCreate([
+                    'name' => $query->name
+                ]);
+            }
         });
+    }
+    
+    protected function isUsingMedicalItem(): bool{
+        $configs = config('module-medical-item.is_using_medical_items',[]);
+        return in_array($this->getMorphClass(), $configs);
     }
 
     public function medicalItem()
